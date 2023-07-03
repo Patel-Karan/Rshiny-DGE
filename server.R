0aae3508a558f8b84003c2d05dd81b8d793a542c
Karan Patel <71623463+Patel-Karan@users.noreply.github.com>
2023-07-03 19:19:32 +0530

    Add files via upload

--- server.R
+++ server.R
@@ -1,10 +1,18 @@
 library(shiny)
 library(dplyr)
+library(tidyr)
+library(reshape2)
 library(tibble)
 library(DT)
+#library(xlsx)
 library(plotly)
+library(ggplot2)
+library(ggdendro)
 
 options(shiny.maxRequestSize=30*1024^2)
+options(java.parameters = "-Xmx30g")
+
+
 
 function(input, output, session) {
   data <- reactive({
@@ -12,7 +20,7 @@ function(input, output, session) {
     if (is.null(inFile))
       return(NULL)
     tbl <- read.table(inFile$datapath, sep=",", header = TRUE, 
-                      row.names = 1,stringsAsFactors = FALSE)
+                      row.names = 1,stringsAsFactors = FALSE, check.names = FALSE)
     return(tbl)
   })
   
@@ -24,7 +32,15 @@ function(input, output, session) {
     updateSelectInput(session, "case",choices = names(data())) 
   })
   
+  
+  
   df <- eventReactive(input$submit,{
+    source("Scripts/Then.R")
+    validate(
+      need(try(input$file1), "Please upload the Count data in CSV format!") %then%
+      need(try(input$control), "Please select the Control samples!") %then%
+      need(try(input$case), "Please select the Treated/Case Samples!")
+    )
       d_filter <- data() %>% select(c(input$control,input$case))
       nC <- length(input$control)
       nT <- length(input$case)
@@ -35,21 +51,145 @@ function(input, output, session) {
       })
   
   output$table <- renderDT({datatable(df() , filter = "top", 
-                                              options = list(scrollX=TRUE),
+                                              options = list(scrollX=TRUE, 
+                                                             extensions = 'Buttons',
+                                                             buttons = c('copy', 'csv', 'excel', 'pdf')),
                                               rownames = FALSE)})
   
   output$downloadData <- downloadHandler(filename = function() {
-      paste(input$comp, ".csv", sep = "")
+      paste(input$comp,".",input$fformat, sep = "")
     },
     content = function(file) {
-      write.csv(df(), file, row.names = FALSE)
+      if (input$fformat == "csv"){
+        write.csv(df(), file, row.names = FALSE)
+      }else if (input$fformat == "xlsx"){
+        write.xlsx(df(),file, sheetNames="Sheet1", row.names = FALSE, append = FALSE)
+      }else if (input$fformat == "txt"){
+        write.table(df(), file, sep = "\t", quote = FALSE, row.names = FALSE)
+      }
     }
   )
   
-  output$volcano <- renderPlotly({
-    plot_ly(data=df()%>%filter(padj != "NA"), x=~log2FoldChange, y=~(-log2(padj)), mode="markers",type = "scatter") %>% 
-      layout(title = "Volcano Plot", xaxis = list(title = 'Fold Change'), 
-             yaxis = list(title = '-log(FDR)'))
-  }) 
+  pvol <- eventReactive(input$plotvol,{
+    df2 <- df() %>% filter(log2FoldChange != "NA")
+    df2$Significant <- "Not-Significant"
+    df2$Significant[df2$log2FoldChange > 1 & df2$pvalue < 0.05] <- "Upregulated"
+    df2$Significant[df2$log2FoldChange < -1 & df2$pvalue < 0.05] <- "Downregulated"
+    source("Scripts/Volcano_Plot.R", local = TRUE)
+    plotv <- pltvol(df2)
+    return(plotv)
+  })
   
-  }
+  output$volcano <- renderPlotly({ggplotly(pvol(),tooltip = c("text"))}) 
+  
+  output$downloadPlotvol <- downloadHandler(filename = function() {
+    paste(input$comp,"_Volcano",".",input$vformat, sep = "")
+  },
+  content = function(file) {
+    if (input$vformat == "png"){
+      ggsave(file, pvol(),device = input$vformat,
+             width = input$vol_width, height = input$vol_height, 
+             limitsize = FALSE, bg= "white", dpi = 250)
+    }else if (input$vformat == "jpeg"){
+      ggsave(file, pvol(),device = input$vformat,
+             width = input$vol_width, height = input$vol_height, 
+             limitsize = FALSE, bg= "white", dpi = 250)
+    }else if (input$vformat == "pdf"){
+      ggsave(file, pvol(),device = input$vformat,
+             width = input$vol_width, height = input$vol_height, 
+             limitsize = FALSE, bg= "white", dpi = 250)
+    }
+  })
+  
+  observeEvent(data(),{
+    updateSelectInput(session, "hclust_Samples",choices = append("All",names(data()))) 
+  })
+  
+  df_hclust <- eventReactive(input$run_hclust,{
+    source("Scripts/Then.R")
+    validate(
+      need(try(input$file1), "Please upload the Count data in CSV format in Analysis Section!") %then%
+      need(try(input$hclust_Samples), "Please select either All or Samples !")
+    )
+    if ((length(input$hclust_Samples)==1) & ("All" %in% input$hclust_Samples)){
+      if (length(names(data()))/2 == 0){
+        nC <- length(names(data()))/2
+        nT <- length(names(data()))-nC
+        source("Scripts/dexp_hclust.R", local = TRUE)
+        dexp_hclust <- d_exp(data(),nC,nT,input$hmethod,input$dmethod)
+      }else{
+        nC <- round(length(names(data()))/2)
+        nT <- length(names(data()))-nC
+        source("Scripts/dexp_hclust.R", local = TRUE)
+        dexp_hclust <- d_exp(data(),nC,nT,input$hmethod,input$dmethod)
+      }
+    }else if((length(input$hclust_Samples)>1) & ("All" %in% input$hclust_Samples)){
+      dexp_hclust <- NULL
+    }else{
+      d_filter <- data() %>% select(c(input$hclust_Samples))
+      if (length(names(d_filter))/2 == 0){
+        nC <- length(names(d_filter))/2
+        nT <- length(names(d_filter))-nC
+        source("Scripts/dexp_hclust.R", local = TRUE)
+        dexp_hclust <- d_exp(d_filter,nC,nT,input$hmethod,input$dmethod)
+      }else{
+        nC <- round(length(names(d_filter))/2, digits = 0)
+        nT <- length(names(d_filter))-nC
+        source("Scripts/dexp_hclust.R", local = TRUE)
+        dexp_hclust <- d_exp(d_filter,nC,nT,input$hmethod,input$dmethod)
+      }
+    }
+    return(dexp_hclust)
+  })
+  
+  output$table2 <- renderDT({
+    validate(
+      need(df_hclust()$d, 'Please select either All or Samples in multiples!'),
+    )
+    datatable( df_hclust()$distance, filter = "top", options = list(scrollX=TRUE, rownames = FALSE))
+    })
+  
+  output$dendoplot <- renderPlotly({
+    validate(
+      need(df_hclust()$d, 'Please select either All or Samples in multiples!'),
+    )
+    dendop <- ggdendrogram(df_hclust()$model$m, rotate = FALSE, leaf_labels = FALSE, size = 2)
+    ggplotly(dendop)
+  })
+  
+  output$pca <- renderPlotly(({
+    validate(
+      need(df_hclust()$d, 'Please select either All or Samples in multiples!'),
+    )
+    scores2 = df_hclust()$pca$pca_score
+    pca_plot <- plot_ly(scores2, x =scores2$PC1 , y = scores2$PC2, 
+                        text = rownames(scores2),mode = "markers",marker = list(size = 11))
+    pca_plot <- layout(pca_plot,
+                       xaxis = list(title=paste("PC1 (", df_hclust()$pca$p_percent[1], "%)", sep = "")),
+                       yaxis = list(title=paste("PC2 (", df_hclust()$pca$p_percent[2], "%)", sep = "")))
+    ggplotly(pca_plot, tooltip = c("text"))
+  }))
+  
+  output$boxplot <- renderPlotly({
+    if (input$hclust_Samples == "All"){
+      box_plot <- df_hclust()$model$df %>% 
+        pivot_longer(1:length(names(data())),names_to="Sample",values_to = "RNC") %>%
+        ggplot(aes(x=Sample,y=-log10(RNC),fill=Sample))+
+        geom_boxplot(alpha=0.3)+
+        theme_bw()+
+        labs(x="Samples",y="Raw Normalized Counts")+
+        theme(legend.title = element_blank(),plot.title = element_text(size=20, face="bold"))
+      ggplotly(box_plot)
+    }else{
+      box_plot <- df_hclust()$model$df %>% 
+        pivot_longer(1:length(names(input$hclust_Samples)),names_to="Sample",values_to = "RNC") %>%
+        ggplot(aes(x=Sample,y=-log10(RNC),fill=Sample))+
+        geom_boxplot(alpha=0.3)+
+        theme_bw()+
+        labs(x="Samples",y="Raw Normalized Counts")+
+        theme(legend.title = element_blank(),plot.title = element_text(size=20, face="bold"))
+      ggplotly(box_plot)
+    }
+    
+  })
+}
--- ui.R
+++ ui.R
@@ -4,30 +4,82 @@ library(shinythemes)
 library(shinycssloaders)
 library(plotly)
 
-# Define UI for application that draws a histogram
-fluidPage(theme = shinytheme("united"),
-    titlePanel("Differential Expression Analysis"),
-    navbarPage(NULL,tabPanel("Analysis",
+fluidPage(theme = shinytheme("cosmo"),includeCSS("www/styles.css"),
+    #titlePanel("Differential Expression Analysis"),
+    navbarPage(title="Logo",tabPanel("Analysis",
                              sidebarLayout(
                                sidebarPanel(
-                                 fileInput("file1", "Upload file in CSV"),
+                                 fileInput("file1", "Upload file in CSV", accept = ".csv", 
+                                           placeholder = "Enter Count table in csv"),
                                  radioButtons("tool", "Tool", c("DESeq2", "edgeR")),
                                  selectInput("control", "Control", character(0), multiple = TRUE),
                                  selectInput("case", "Case", character(0), multiple = TRUE),
-                                 textInput("comp","Comparision","Control_vs_Case"),
                                  actionButton("submit","Run"),
-                                 downloadButton("downloadData", "Download")
                                ),
                                mainPanel(
+                                 fluidRow(
+                                   column(4,
+                                          selectInput("fformat","Select File Format",
+                                                      choices = c("csv","xlsx","txt"), selected = "csv")
+                                   ),
+                                   column(4,textInput("comp","Enter Comparision Name","Control_vs_Case")),
+                                   column(4, tags$br(),downloadButton("downloadData", "Download", 
+                                                            icon = icon("download")))
+                                 ),
                                  withSpinner(
                                    DTOutput(outputId = 'table')
                                  ))
                              )
                              ),
-               tabPanel("Hierarchical Clustering & Correlation Analysis"),
+               tabPanel("Hierarchical Clustering & Correlation Analysis",
+                        sidebarLayout(sidebarPanel(
+                          selectInput("dmethod","Select distance method", 
+                                      choices = c("euclidean","maximum","manhattan",
+                                                  "canberra","binary","minkowski"),
+                                      selected="euclidean"),
+                          selectInput("hmethod", "Select hclust methods", 
+                                      choices = c("ward.D", "ward.D2", "single", "complete", 
+                                                  "average", "mcquitty", "median", "centroid"),
+                                      selected = "complete"),
+                          selectInput("hclust_Samples","Select Samples for Hclust"
+                                      ,character(0), multiple = TRUE),
+                          actionButton("run_hclust","Run")
+                        ),
+                        mainPanel(
+                          navbarPage(NULL,
+                                     tabPanel("Distance Matrix",
+                                              withSpinner(
+                                                DTOutput(outputId = 'table2')
+                                              )
+                                              ),
+                                     tabPanel("Dendogram",
+                                              withSpinner(
+                                                plotlyOutput("dendoplot")
+                                              )),
+                                     tabPanel("PCA",
+                                              withSpinner(
+                                       plotlyOutput("pca")
+                                     )),
+                                     tabPanel("BoxPlot",
+                                              withSpinner(
+                                                plotlyOutput("boxplot")
+                                              )),
+                                     tabPanel("Correlation"))
+                        ))),
                tabPanel("Volcano Plot",
+                        fluidRow(column(3,
+                                        tags$br(),actionButton("plotvol", "Generate Plot", width = "80%"),
+                        ),
+                        column(3,selectInput("vformat","Select File Format",
+                                             choices = c("png","jpeg","pdf"), selected = "png")
+                        ),
+                        column(3,numericInput("vol_width", "Width in px",13),
+                               numericInput("vol_height", "Height in px",7)),
+                        column(3,tags$br(), downloadButton("downloadPlotvol", "Download",
+                                                icon = icon("download")))
+                        ),tags$br(),
                         withSpinner(
-                          plotlyOutput("volcano")
+                          plotlyOutput("volcano", height = "100%", width = "80%")
                         )),
                tabPanel("Heatmaps"),
                tabPanel("About")
